003b2650dd2f4c26139a9ba06dd2c88c6b4d4825
Adrian Tiberius <adrian.tiberius@gmail.com>
2015-06-04 16:30:55 +0200

    Some lint fixes.

--- app/src/main/java/org/ethereum/android_app/MainActivity.java
+++ app/src/main/java/org/ethereum/android_app/MainActivity.java
@@ -62,7 +62,7 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        Log.v(TAG, new Integer(id).toString());
+        Log.v(TAG, Integer.valueOf(id).toString());
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
