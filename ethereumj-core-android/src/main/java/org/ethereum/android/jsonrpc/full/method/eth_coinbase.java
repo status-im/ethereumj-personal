df60b74193fe3d7a362656ffa6593f3688c6add9
Yaroslav Dmytrotsa <dmytyar@vitgroup.com>
2015-07-06 10:25:18 +0300

    typo

--- ethereumj-core-android/src/main/java/org/ethereum/android/jsonrpc/full/method/eth_coinbase.java
+++ ethereumj-core-android/src/main/java/org/ethereum/android/jsonrpc/full/method/eth_coinbase.java
@@ -18,7 +18,7 @@ public class eth_coinbase extends JsonRpcServerMethod {
 
     protected JSONRPC2Response worker(JSONRPC2Request req, MessageContext ctx) {
 
-        String tmp = "0x" + getCoinBase();
+        String tmp = "0x" + Hex.toHexString(getCoinBase());
         JSONRPC2Response res = new JSONRPC2Response(tmp, req.getID());
         return res;
 
