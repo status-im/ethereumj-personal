b76a72f367d742b1c587173562ea1a002aa436e6
Adrian Tiberius <adrian.tiberius@gmail.com>
2015-08-12 17:29:07 +0200

    Readded parcelable fix.

--- ethereumj-core-android/src/main/java/org/ethereum/android/service/events/BlockEventData.java
+++ ethereumj-core-android/src/main/java/org/ethereum/android/service/events/BlockEventData.java
@@ -7,7 +7,7 @@ import android.os.Parcelable;
 import org.ethereum.core.Block;
 import org.ethereum.core.TransactionReceipt;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BlockEventData extends EventData {
@@ -60,6 +60,10 @@ public class BlockEventData extends EventData {
 
         super(in);
         block = in.readParcelable(org.ethereum.android.interop.Block.class.getClassLoader());
-        receipts = Arrays.asList((TransactionReceipt[])in.readParcelableArray(TransactionReceipt.class.getClassLoader()));
+        receipts = new ArrayList<>();
+        Parcelable[] items = in.readParcelableArray(TransactionReceipt.class.getClassLoader());
+        for (Parcelable item: items) {
+            receipts.add((TransactionReceipt)item);
+        }
     }
 }
